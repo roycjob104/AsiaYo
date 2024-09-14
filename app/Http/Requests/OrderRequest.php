fa4b5248e2a0cbee4e3dbd9c0ba85acd55046570
Roy Chang <roycjob104@gmail.com>
2024-09-14 16:41:24 +0800

    add Order Request

---
+++ app/Http/Requests/OrderRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Enum;
+use Illuminate\Validation\Rule;
+use App\Enums\CurrencyEnum;
+
+class OrderRequest extends BaseFormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules()
+    {
+        return [
+            'id' => 'required|string',
+            'name' => 'required|string|max:255',
+            'address.city' => 'required|string|max:255',
+            'address.district' => 'required|string|max:255',
+            'address.street' => 'required|string|max:255',
+            'price' => 'required|numeric',
+            'currency' => ['required', Rule::in(CurrencyEnum::values())],
+        ];
+    }
+}
