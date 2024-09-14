c0de729191659b81fea6675d6aa4188f7722345e
Roy Chang <roycjob104@gmail.com>
2024-09-14 16:43:18 +0800

    update: morph map

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Define the morph map
+        Relation::morphMap([
+            'order_twd' => 'App\Models\Order\Currencies\OrderTwdModel',
+            'order_usd' => 'App\Models\Order\Currencies\OrderUsdModel',
+            'order_jpy' => 'App\Models\Order\Currencies\OrderJpyModel',
+            'order_rmb' => 'App\Models\Order\Currencies\OrderRmbModel',
+            'order_myr' => 'App\Models\Order\Currencies\OrderMyrModel',
+        ]);
     }
 }
